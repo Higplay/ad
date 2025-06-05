1d87dbcf301fef53357558e2b0df7cfa1765e642
Higplay <145916475+Higplay@users.noreply.github.com>
2025-06-05 18:19:42 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome To React.js</h1>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>React</li>
+      </ul>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea explicabo temporibus unde tenetur consequuntur possimus nisi eius, voluptatum in ipsa.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima ducimus, vero porro qui, provident voluptatum asperiores omnis corporis sed maiores pariatur iste quasi ut repellat obcaecati culpa labore reprehenderit vitae distinctio. Impedit sapiente odio iure recusandae similique nihil obcaecati commodi.</p>
+    </div>
+  )
+}
+
+export default App
